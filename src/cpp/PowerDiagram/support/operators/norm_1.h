ee7d26368c44472b2969148690529f232c202e75
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-08-06 22:01:19 +0200

    n1

--- src/cpp/PowerDiagram/support/operators/norm_1.h
+++ src/cpp/PowerDiagram/support/operators/norm_1.h
@@ -5,10 +5,10 @@
 BEG_METIL_NAMESPACE
 
 /// norm 1
-auto norm_1( auto &&a ) -> std::decay_t<decltype( abs( a[ 0 ] ) + abs( a[ 1 ] ) )> {
+auto norm_1( const auto &a ) -> std::decay_t<decltype( abs( a[ 0 ] ) )> {
     if ( a.size() == 0 )
         return 0;
-    using TR = std::decay_t<decltype( abs( a[ 0 ] ) + abs( a[ 1 ] ) )>;
+    using TR = std::decay_t<decltype( abs( a[ 0 ] ) )>;
     TR res = abs( a[ 0 ] );
     for( std::size_t i = 1; i < std::size_t( a.size() ); ++i )
         res += abs( a[ i ] );
