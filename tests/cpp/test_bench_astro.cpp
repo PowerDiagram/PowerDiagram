1565d5bd4d019a6ba7fa3b992f87caae93b237b9
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-10-13 19:29:11 +0200

    .

--- tests/cpp/test_bench_astro.cpp
+++ tests/cpp/test_bench_astro.cpp
@@ -1,3 +1,4 @@
+#include <tl/support/operators/sum.h>
 #include "PowerDiagram/PowerDiagram.h"
 #include "catch_main.h"
 #include <fstream>
@@ -14,7 +15,7 @@ void bench( Str filename ) {
     Vec<Pt> positions;
     Vec<TF> weights;
     if ( filename.ends_with( ".xyz32.bin" ) ) {
-        for( PI i = 0; i < ( 1 << 22 ); ++i ) {
+        for( PI i = 0;; ++i ) {
             float x, y, z;
             f.read( (char *)&x, sizeof( x ) );
             f.read( (char *)&y, sizeof( y ) );
@@ -25,16 +26,20 @@ void bench( Str filename ) {
             weights << 0;
         }
     } else {
-        for( PI i = 0; ; ++i ) {
-            double x, y, z;
-            f >> x >> y >> z;
-            if ( ! f )
-                break;
-            positions << Pt{ x, y, z };
+        for( PI i = 0; i < 16777216; ++i ) {
+            positions << Pt{ rand() / RAND_MAX, rand() / RAND_MAX, rand() / RAND_MAX };
             weights << 0;
         }
+        // for( PI i = 0; ; ++i ) {
+        //     double x, y, z;
+        //     f >> x >> y >> z;
+        //     if ( ! f )
+        //         break;
+        //     positions << Pt{ x, y, z };
+        //     weights << 0;
+        // }
     }
-    // P( positions.size() );
+    P( positions.size() );
 
     // boundaries
     Vec<TF> bnd_offs;
@@ -55,9 +60,11 @@ void bench( Str filename ) {
 
     auto t0 = std::chrono::steady_clock::now();
 
+    Vec<TF> volumes( FromSizeAndItemValue(), pd.max_nb_threads(), 0 );
     Vec<PrevCutInfo<Config>> prev_cuts( FromSize(), pd.nb_cells() );
     pd.for_each_cell( [&]( Cell<Config> &cell, int num_thread ) {
         cell.get_prev_cut_info( prev_cuts[ cell.i0 ] );
+        // volumes[ num_thread ] += cell.measure();
     } );
 
     auto t1 = std::chrono::steady_clock::now();
@@ -68,8 +75,10 @@ void bench( Str filename ) {
 
     auto t2 = std::chrono::steady_clock::now();
     std::cout << std::chrono::nanoseconds( t2 - t1 ).count() / 1e6 << "ms" << std::endl;
+
+    // P( sum( volumes ) );
 }
 
 TEST_CASE( "bench Astro 3D", "" ) {
-    bench( "16M.xyz32.bin" );
+    bench( "100M_60Mpch_jittered.xyz32.bin" );
 }
