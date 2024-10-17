2f9747474b5c26b9a8833e48e7c08a5710ceb401
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-10-17 19:57:00 +0200

    .

--- src/python/PowerDiagram/__init__.py
+++ src/python/PowerDiagram/__init__.py
@@ -1,13 +1,28 @@
-# cpp source directory
-def add_cpp_flags():
-    from pathlib import Path
-    import vfs
+# # cpp source directory
+# def add_cpp_flags():
+#     from pathlib import Path
+#     import vfs
 
-    vfs.add_global_cpp_flag( "-I" + str( Path( __file__ ).parents[ 3 ].joinpath( 'src', 'cpp' ) ) )
-    vfs.add_global_cpp_flag( "--cpp-flag=-O3" )
+#     vfs.add_global_cpp_flag( "-I" + str( Path( __file__ ).parents[ 3 ].joinpath( 'src', 'cpp' ) ) )
+#     vfs.add_global_cpp_flag( "--cpp-flag=-O3" )
 
-add_cpp_flags()
+# add_cpp_flags()
 
-# exports
-from .ConvexFunction import ConvexFunction
-from .PowerDiagram import PowerDiagram
+# # exports
+# from .ConvexFunction import ConvexFunction
+# from .PowerDiagram import PowerDiagram
+# load the shared library
+# import cppimport
+from pathlib import Path
+import sys
+
+def test_file( filename, content ):
+    pass
+
+def lib( scalar_type, nb_dims ):
+    content = """
+        module ( pouet ) {
+        }
+    """
+    test_file( str( Path( __file__ ) / "bindings" / f"bindings_{ scalar_type }_{ nb_dims }.cpp" ), content )
+    
